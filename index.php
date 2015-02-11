2b7cd868a960684ffa6bd99d2275f6cade34f9df
seb <seb@sebLaptop>
2015-02-11 23:45:07 +0100

    modif mep

--- index.php
+++ index.php
@@ -2,9 +2,11 @@
 session_start();
 require_once("include/fct.inc.php");
 require_once ("include/class.pdogsb.inc.php");
-include("vues/v_entete.php") ;
+include("vues/v_entete.php") ; ?>
+<script src ="js/jquery-1.11.2.min.js" type="text/javascript"></script>
+<script src ="js/chainedList.js" type="text/javascript"></script>
 
-
+<?php
 $pdo = PdoGsb::getPdoGsb();
 $estConnecte = estConnecte();
 //print_r($_SESSION); // DEBUG
