4f32dd8f9e2f6bb6725510c41da9d37edac6270c
seb <seb@sebLaptop>
2015-02-11 23:44:43 +0100

    modif mep

--- vues/v_sommaire_comptable.php
+++ vues/v_sommaire_comptable.php
@@ -13,7 +13,7 @@
 				<?php echo $_SESSION['prenom']."  ".$_SESSION['nom']  ?>
 			</li>
            <li class="smenu">
-              <a href="index.php?uc=validerFrais&action=fuck" title="Saisie fiche de frais ">Validation fiche de frais</a>
+              <a href="index.php?uc=validerFrais&action=selectionnerVisiteur" title="Saisie fiche de frais ">Validation fiche de frais</a>
            </li>
            <li class="smenu">
               <a href="index.php?uc=suivreFrais&action=fuck2" title="Consultation de mes fiches de frais">Suivi paiment fiche de frais</a>
@@ -24,4 +24,4 @@
          </ul>
         
     </div>
-    
+                                                   
