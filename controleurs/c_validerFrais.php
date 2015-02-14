405b4beffeabef0ca069cc57e716d87ba349b3c0
seb <seb@sebLaptop>
2015-02-14 15:24:13 +0100

    modif report de frais

--- controleurs/c_validerFrais.php
+++ controleurs/c_validerFrais.php
@@ -1,5 +1,4 @@
-<?php
-
+        <?php 
 if (($_SESSION['poste']) == 'comptable') {
     include("vues/v_sommaire_comptable.php");
     $idComptable = $_SESSION['idVisiteur'];
@@ -22,23 +21,41 @@ if (($_SESSION['poste']) == 'comptable') {
     } else {
         $mois = "";
     }
+
     switch ($action) {
 
         case 'selectionnerVisiteur': {
+            $lesMois = $pdo->dernierMoisCloture($idVisiteur);
+                var_dump($lesMois);
                 $lesVisiteurs = $pdo->getVisiteur();
                 include 'vues/v_listeVisiteurs.php';
+                
                 break;
             }
 
         case 'selectionnerMois': {
 
-                $lesMois = $pdo->getLesMoisDisponibles($idVisiteur);
+                $lesMois = $pdo->dernierMoisCloture($idVisiteur);
+                if($lesMois == null){
+                    ajouterErreur("Pas de fiche de frais pour ce visiteur ce mois");
+                    include("vues/v_erreurs.php");
+                    //echo '<meta http-equiv="Refresh" content="timer;url=Prod/gsbMVC/index.php?uc=validerFrais&action=selectionnerVisiteur">';
+                    //header('Location: index.php?uc=validerFrais&action=selectionnerVisiteur'); 
+                }else{
+                                    var_dump($lesMois);
 
                 include 'vues/v_listeVisiteurs_mois.php';
+                }
+
                 break;
             }
 
         case 'validerMajFraisForfaitComptable': {
+            
+            include('vues/v_validerFiche.php');
+            
+                include ("vues/v_ficheVisiteur.php");  
+
                 if (isset($_REQUEST['lesFrais'])) {
                     $lesFrais = $_REQUEST['lesFrais'];
                     ajouterValideOk("Le changement des frais a été validé");
@@ -63,22 +80,59 @@ if (($_SESSION['poste']) == 'comptable') {
                 break;
             }
             
-        case 'validerCreationFrais': {
-                $dateFrais = $_REQUEST['dateFrais'];
-                $libelle = $_REQUEST['libelle'];
-                $montant = $_REQUEST['montant'];
-                valideInfosFrais($dateFrais, $libelle, $montant);
-                if (nbErreurs() != 0) {
+            
+        case 'supprimerFraisComptable': {
+            $idVisiteur = $_SESSION['visitor'];
+                $mois = $_SESSION['moa'];
+                $idFrais = $_REQUEST['idFrais'];
+                $tailleLibelle = $_REQUEST['tailleLibelle'];
+                $debutLibelle = $_REQUEST['debutLibelle'];
+                if(strcmp($debutLibelle,'REFUSE')){
+                    if($tailleLibelle + 7 > 100){
+                        $pdo->refuserFraisHorsForfaitTronque($idFrais);
+                    }else{
+                        $pdo->refuserFraisHorsForfait($idFrais);
+                    }
+                }
+                else{
+                    ajouterErreur("Le Frais a déjà été refusé");
                     include("vues/v_erreurs.php");
-                } else {
-                    $pdo->creeNouveauFraisHorsForfait($idVisiteur, $mois, $libelle, $dateFrais, $montant);
                 }
                 break;
             }
-            
-        case 'supprimerFraisComptable': {
+        
+        case 'reporterFraisComptable': {
+            $idVisiteur = $_SESSION['visitor'];
+                $mois = $_SESSION['moa'];
+                $moisProchain = ajouterMois($mois);
+                
                 $idFrais = $_REQUEST['idFrais'];
-                $pdo->supprimerFraisHorsForfait($idFrais);
+                $libelle = $_REQUEST['libelle'];
+			$date = $_REQUEST['date'];
+			$montant=$_REQUEST['montant'];
+
+               
+                    $pdo->supprimerFraisHorsForfait($idFrais); 
+                    if($pdo->estPremierFraisMois($idVisiteur,$moisProchain)){
+                        $pdo->creeNouvellesLignesFrais($idVisiteur,$moisProchain);
+                        $pdo->creeNouveauFraisHorsForfait($idVisiteur,$moisProchain,$libelle,$date,$montant);
+                    }else{
+                        $pdo->creeNouveauFraisHorsForfait($idVisiteur,$moisProchain,$libelle,$date,$montant); 
+                    }
+                
+                
+                break;
+            }
+            
+        case 'validerFiche': {
+                $idVisiteur = $_SESSION['visitor'];
+                $mois = $_SESSION['moa'];
+                var_dump($idVisiteur);
+                var_dump($mois);
+                $pdo->majEtatFicheFrais($idVisiteur,$mois,'VA');
+                
+            
+            
                 break;
             }
     }
