e0943d64a17714e2c7ce5d71c1cea11d052550f3
seb <seb@sebLaptop>
2015-02-14 15:29:21 +0100

    modif mep

---
+++ vues/v_listeFraisForfait_comptable.php
@@ -0,0 +1,43 @@
+
+<div id="contenu">
+      <h2>Modifier la fiche de <?php echo substr(($_SESSION['moa']),4,2)."/". substr(($_SESSION['moa']),0,4) ." du visiteur ".$pdo->getNomPrenomVisiteur($idVisiteur)['nom']. ' ' .$pdo->getNomPrenomVisiteur($idVisiteur)['prenom']; ?></h2> 
+       
+      <form method="POST"  action="index.php?uc=validerFrais&action=validerMajFraisForfaitComptable">
+      <div class="corpsForm">
+          
+          <fieldset>
+              <legend>Eléments forfaitisés
+              </legend>
+              <?php
+              foreach ($lesFraisForfait as $unFrais) {
+                  $idFrais = $unFrais['idfrais'];
+                  $libelle = $unFrais['libelle'];
+                  $quantite = $unFrais['quantite'];
+
+                  ?>
+                  <p>
+                      <label for="idFrais"><?php echo $libelle ?></label>
+                      
+                      <input type="text" id="idFrais" name="lesFrais[<?php echo $idFrais ?>]" size="10" maxlength="5" value="<?php echo $quantite ?>" >
+                      
+                  </p>
+
+                  <?php
+              }
+              ?>
+
+
+
+
+
+          </fieldset>
+      </div>
+      <div class="piedForm">
+      <p>
+        <input id="ok" type="submit" value="Valider" size="20" />
+        <input id="annuler" type="reset" value="Effacer" size="20" />
+      </p> 
+      </div>
+        
+      </form>
+  
